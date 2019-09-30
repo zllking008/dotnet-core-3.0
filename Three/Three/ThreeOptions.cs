3548b48dc1c5c1276fa6658856f59b3fffd61b72
zhaokunming <sgzll_007@126.com>
2019-09-30 16:21:25 +0800

    * 配置信息 appsettings的调用 viewcomponent 的运用
    文件补充

---
+++ Three/Three/ThreeOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Three
+{
+    public class ThreeOptions
+    {
+        /// <summary>
+        /// 这里映射的是appsettings.json中的 Three.BoldDepartmentEmployeeCountThreshold节点
+        /// </summary>
+        public int BoldDepartmentEmployeeCountThreshold { get; set; }
+    }
+}
---
+++ Three/Three/ViewComponents/CompanySummaryViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Three.Services;
+
+namespace Three.ViewComponents
+{
+    public class CompanySummaryViewComponent:ViewComponent
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public CompanySummaryViewComponent(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Title"></param>
+        /// <returns></returns>
+        public async Task<IViewComponentResult> InvokeAsync(string Title)
+        {
+            ViewBag.Title = Title;
+            var summary = await _departmentService.GetCompanySummary();
+            return View(summary);
+        }
+    }
+}
