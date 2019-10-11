0865d507f1ea9fd22bfdb07e957491c89603cf64
zhaokunming <sgzll_007@126.com>
2019-10-11 18:31:42 +0800

    signalR 原理 razor page 2

---
+++ Three/ThreePage/Pages/Department/AddDepartment.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Three.Services;
+
+namespace ThreePage.Pages.Department
+{
+    public class AddDepartmentModel : PageModel
+    {
+        private readonly IDepartmentService _departmentService;
+
+        [BindProperty]
+        public Three.Models.Department Department { get; set; }
+        //public void OnGet()
+        //{
+        //}
+        public AddDepartmentModel(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _departmentService.Add(Department);
+            return RedirectToPage("/Index");
+
+        }
+    }
+}
